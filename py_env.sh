479584bd71fc133b163a7f2a92a7c05c8faa5491
root <root@pbmcflow-0.pbmcflow.default.svc.cluster.local>
2025-05-23 11:14:53 -0700

    first commit

---
+++ py_env.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+PY_ENV="/home/workspace/environment/mm_py_env"
+
+# Create conda environment with Python and R packages
+conda create -y -p $PY_ENV -c conda-forge \
+    python=3.9 \
+    ipykernel jupyterlab \
+    numpy==1.24.4 pandas \
+    scipy scikit-learn \
+    matplotlib seaborn plotly \
+    tqdm requests openpyxl h5py \
+    dill
+
+# Activate the environment
+conda activate $PY_ENV
+
+# Install the Jupyter kernel
+python -m ipykernel install --user --name=mm_py_env --display-name="Python (PBMC Flow)"
